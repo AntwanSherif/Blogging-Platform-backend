b5b9ddcb636468436ac6284b13cbdfd435b82f08
Antwan Sherif <antwansherif@gmail.com>
2020-05-13 20:47:24 +0200

    feat: Add signup route and controller and create User model

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ require('dotenv').config();
 
 //routes
 const blogRoutes = require('./routes/blog');
+const authRoutes = require('./routes/auth');
 
 //app
 const app = express();
@@ -28,6 +29,7 @@ if(process.env.NODE_ENV === 'development') {
 
 //routes middleware
 app.use('/api', blogRoutes);
+app.use('/api', authRoutes);
 
 //port
 const port = process.env.PORT || 3001;
