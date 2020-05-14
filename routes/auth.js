f4e767e3f187741f6047abf5fe20a4d9ce380902
AntwanSherif <antwansherif@gmail.com>
2020-05-14 18:48:19 +0200

    feat: User signup

---
+++ routes/auth.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { signup } = require('../controllers/auth');
+
+//validators
+const { runValidation } = require('../validators/index');
+const { userSignupValidator } = require('../validators/auth');
+
+router.post('/signup', userSignupValidator, runValidation, signup);
+
+module.exports = router;
