fa32bae46dd1bc1365eda003073dcfaf4b93cfa4
AntwanSherif <antwansherif@gmail.com>
2020-05-14 18:43:49 +0200

    feat: signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,24 @@
+const User = require('../models/user');
+const shortId = require('shortid');
+
 exports.signup = (req, res) => {
-    const { name, email, password } = req.body;
-    
-    res.json({ user: { name, email, password }})
+    User.findOne({ email: req.body.email }).exec((err, user) => {
+        if (user) {
+            return res.send(400).json({ error: 'Email is already registered' })
+        }
+
+        //create a new user
+        const { name, email, password } = req.body;
+        const username = shortId.generate();
+        const profile = `${process.env.CLIENT_URL}/profile/${username}`;
+
+        const newUser = new User({ username, name, email, password, profile });
+        newUser.save((error, success) => {
+            if (error) {
+                return res.status(400).json({ error });
+            }
+
+            res.json({ message: 'Signup success!' });
+        })
+    })
 }
--- models/user.js
+++ models/user.js
@@ -75,7 +75,7 @@ userSchema.methods = {
         }
     },
     makeSalt: function () {
-        return Math.round(new Date().valueOf * Math.random()) + '';
+        return Math.round(new Date().valueOf() * Math.random()) + '';
     },
     authenticate: function (plainPassword) {
         return this.encryptPassword(plainPassword) === this.hashedPassword
