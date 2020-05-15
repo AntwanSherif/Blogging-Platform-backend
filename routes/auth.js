e261c066ca9776d6887c2a9d3371b61f2086d2fc
AntwanSherif <antwansherif@gmail.com>
2020-05-15 10:22:11 +0200

    feat: Handle login/logout

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const { signup } = require('../controllers/auth');
+const { signup, login, logout, requireLogin } = require('../controllers/auth');
 
 //validators
 const { runValidation } = require('../validators/index');
-const { userSignupValidator } = require('../validators/auth');
+const { signupValidator, loginValidator } = require('../validators/auth');
 
-router.post('/signup', userSignupValidator, runValidation, signup);
+router.post('/signup', signupValidator, runValidation, signup);
+router.post('/login', loginValidator, runValidation, login);
+router.get('/logout', logout);
+
+//test
+router.get('/secret', requireLogin, (req, res) => {
+    res.json({ message: 'you have access to this protected page' });
+});
+
 
 module.exports = router;
--- validators/auth.js
+++ validators/auth.js
@@ -1,7 +1,12 @@
 const { check } = require('express-validator');
 
-exports.userSignupValidator = [
+exports.signupValidator = [
     check('name').not().isEmpty().withMessage('Name is required'),
     check('email').isEmail().withMessage('Invalid email address'),
     check('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
-]
+];
+
+exports.loginValidator = [
+    check('email').isEmail().withMessage('Invalid email address'),
+    check('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
+];
